dfe4138fb0bea2f0c8dfcb8f55b1a1c734172af2
Srinivasareddymediboina <47879164+Srinivasareddymediboina@users.noreply.github.com>
2019-04-17 16:09:29 +0530

    Update maxnumber.py

--- maxnumber.py
+++ maxnumber.py
@@ -1,7 +1,9 @@
+# input number will be taken by input keyword
 first= int(input("Enter first number: "))
 second= int(input("Enter second number: "))
 third= int(input("Enter third number: "))
- 
+
+#using the conditions
 if (first > second) and (first > third):
    maximum = first
 elif (second > first) and (second > third):
