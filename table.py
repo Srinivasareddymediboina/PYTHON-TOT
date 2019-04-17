7fb6881885f7d0549c2ef6cda844172aaf6e2e8c
Srinivasareddymediboina <47879164+Srinivasareddymediboina@users.noreply.github.com>
2019-04-17 16:05:40 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,7 +1,9 @@
 #Enter the number 
 n=int(input("enter tables"))
 
+#single table
 for i in range(1,n+1):
+    #tables up to given range
     for a in range(1,11):
         print(i,"x",a,"=",a*i)
         
