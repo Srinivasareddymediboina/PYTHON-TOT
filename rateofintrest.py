12566cd9a3a8a5922616fb6033436535a2c7a262
Srinivasareddymediboina <47879164+Srinivasareddymediboina@users.noreply.github.com>
2019-04-17 16:14:59 +0530

    Add files via upload

---
+++ rateofintrest.py
@@ -0,0 +1,5 @@
+amount=float(input("Enter the total amount:"))
+time=int(input("Enter the time(years):"))
+rate=float(input("Enter the rate:"))
+interest=(amount*time*rate)/100
+print("The simple interest is:",interest)
