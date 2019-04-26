4f9f1527cead9b1c18154585dfadc0db02304952
Srinivasareddymediboina <47879164+Srinivasareddymediboina@users.noreply.github.com>
2019-04-26 10:44:23 +0530

    Add files via upload

--- diiff_date.py
+++ diiff_date.py
@@ -2,7 +2,11 @@ from datetime import date
 y=int(input("enter the year"))
 m=int(input("enter the month"))
 d=int(input("enter the day"))
+y1=int(input("enter the year"))
+m1=int(input("enter the month"))
+d1=int(input("enter the day")
 from_date = date(y, m, d)
-to_date = date(2019, 4, 20)
+
+to_date = date(y1, m1, d1)
 x = to_date - from_date
 print(x.days, "days")
